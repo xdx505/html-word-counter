255a151f9f7afb3842d3586cb242067a198bbb67
Dan Shadrin <shadrin.smr@gmail.com>
2021-09-29 11:09:58 +0400

    Add swagger

---
+++ src/main/java/ru/xdx505/wordcounter/config/PersistenceConfig.java
@@ -0,0 +1,19 @@
+package ru.xdx505.wordcounter.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.auditing.DateTimeProvider;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.time.ZonedDateTime;
+import java.util.Optional;
+
+@Configuration
+@EnableJpaAuditing(dateTimeProviderRef = "auditingDateTimeProvider")
+public class PersistenceConfig {
+
+    @Bean(name = "auditingDateTimeProvider")
+    public DateTimeProvider dateTimeProvider() {
+        return () -> Optional.of(ZonedDateTime.now());
+    }
+}
